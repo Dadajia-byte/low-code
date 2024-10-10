847bd6f9d07ba618dc8a6f1056df2ec0c02bac06
Dadajia-byte <1814650097@qq.com>
2024-10-10 16:02:29 +0800

    feat: 初步实现自动生成api脚本，但是还无法直接使用（没有考虑post请求格式）

--- src/db/queryModel.js
+++ src/db/queryModel.js
@@ -1,35 +1,33 @@
-export default function getModel(storeName, keyPathValue = null, indexName = null, indexValue = null, tranRequest = null) {
+/**
+ * 查询对象
+ * @param {IndexedDBHelp} help 访问数据库的实例
+ * @param {string} storeName 仓库名称（表名）
+ * @param {any} key 主键值
+ * @param {IDBTransaction} tranRequest 如果使用事务的话，需要传递开启事务时创建的连接对象
+ * @returns 查询结果
+ */
+export default function queryModel(help, storeName, key, tranRequest = null) {
+  // 定义一个 Promise 的实例
   return new Promise((resolve, reject) => {
+    // 定义个函数，便于调用
     const _query = (__tran) => {
-      let objectStore;
-      if (indexName && indexValue) {
-        objectStore = __tran.objectStore(storeName).index(indexName);
-        const request = objectStore.get(indexValue);
-        request.onsuccess = (event) => resolve(event.target.result);
-      } else if (keyPathValue !== null) {
-        objectStore = __tran.objectStore(storeName);
-        const request = objectStore.get(keyPathValue);
-        request.onsuccess = (event) => resolve(event.target.result);
-      } else {
-        objectStore = __tran.objectStore(storeName);
-        const request = objectStore.openCursor();
-        const results = [];
-        request.onsuccess = (event) => {
-          const cursor = event.target.result;
-          if (cursor) {
-            results.push(cursor.value);
-            cursor.continue();
-          } else {
-            resolve(results);
-          }
-        };
-      }
-      request.onerror = (err) => reject(err);
+      const store = __tran.objectStore(storeName);
+      const request = store.get(key); // 查询对象
+      request.onsuccess = (event) => {
+        resolve(event.target.result); // 返回查询结果
+      };
+      request.onerror = (event) => {
+        reject(event.target.error); // 失败抛出错误
+      };
     };
 
     if (tranRequest === null) {
-      this.beginRead([storeName]).then((__tran) => _query(__tran));
+      help.beginRead([storeName]).then((tran) => {
+        // 自己开一个事务
+        _query(tran);
+      });
     } else {
+      // 使用传递过来的事务
       _query(tranRequest);
     }
   });
