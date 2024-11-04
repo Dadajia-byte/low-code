6f129abd4a329457ad6b81345b5b871c5b4717fd
Mr-East <113604025+Mr-East@users.noreply.github.com>
2024-11-04 20:59:34 +0800

    feat: 调整网格样式

--- src/packages/Editor/index.vue
+++ src/packages/Editor/index.vue
@@ -201,16 +201,17 @@ const canvas = async () => {
   }
 
   function drawGrid(width, height) {
-    const gridSize = 5; // 网格大小
-    const dotSize = 0.5 ; // 圆点半径
+    const gridSize = 6; // 网格大小
+    const dotSize = 1; // 圆点半径
+    
     ctx.fillStyle = "rgba(0, 0, 0, 0.2)"; // 圆点颜色和透明度
 
-    for (let x = gridSize; x < width; x += gridSize) {
-      for (let y = gridSize; y < height; y += gridSize) {
+    for (let x = 0; x < width; x += gridSize) {
+     
         ctx.beginPath();
         ctx.arc(x, y, dotSize, 0, Math.PI * 2);
         ctx.fill();
-      }
+      
     }
   }
 };
@@ -574,6 +575,7 @@ onMounted(() => {
         position: relative;
         border-radius: 10px;
         border: #e3e3e3 2px dashed;
+        overflow: hidden;
       }
     }
   }
