4084a935d5d016b7893dcb628ace8c15c0bbc55e
Dadajia-byte <1814650097@qq.com>
2024-11-06 19:05:43 +0800

    fix: 部署文件名大小写规范

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 </template>
 <script setup>
-import Editor from "./packages/Editor/index.vue"
 import './assets/icons/iconfont/iconfont.css'
 import { registerConfig as config } from './utils/editorConfig';
 provide('config', config)
